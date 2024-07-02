db7cbdaa45dd64f50d2a6f8c98ec18111358a057
Phoe <dariodebenitopadilla@pm.me>
2024-07-02 22:56:44 +0200

    movimiento de las cartas y timers

--- scripts/card_ui.gd
+++ scripts/card_ui.gd
@@ -6,6 +6,19 @@ func _ready():
 	cartas = get_children()
 
 func colocar_cartas_en_mano(mano):
+	ocultar_cartas(false)
 	var n = mano.size()
 	for i in range(n):
 		cartas[i].set_carta(mano[i])
+	await get_tree().create_timer(1.0).timeout
+	mostrar_cartas()
+
+func mostrar_cartas():
+	for i in cartas:
+		i.bajar_carta()
+
+func ocultar_cartas(no_jugadas : bool):
+	# false para ocultar todas las cartas
+	# true para ocultar solo las no jugadas
+	for i in cartas:
+		i.ocultar_carta(no_jugadas)
--- scripts/carta_mano.gd
+++ scripts/carta_mano.gd
@@ -1,44 +1,63 @@
 extends Control
 
-@onready var card_is_up : bool = false
-@onready var descripcion : Sprite2D = find_child("descripcion")
-@onready var animation_player = find_child("AnimationPlayer")
+@onready var carta_arriba = false
+@onready var carta_jugada = false
+@onready var descripcion = $descripcion
+@onready var animation_player = $AnimationPlayer
 @onready var carta = $carta
+@onready var pos_ini = position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	descripcion.visible = false
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_carta_mano_mouse_entered():
-	# animation_player.play("card_up")
-	position += Vector2.UP * 80
-	card_is_up = true
+	subir_carta()
+	carta_arriba = true
 	descripcion.visible = true
-	pass 
 
 func _on_carta_mano_mouse_exited():
+	if carta_jugada:
+		pass
+	else:
+		bajar_carta()
+		carta_arriba = false
+		descripcion.visible = false
+
+func subir_carta():
+	# animation_player.play("card_up")
+	position = pos_ini + Vector2.UP * 80
+
+func bajar_carta():
 	# animation_player.play_backwards("card_up")
-	card_is_up = false
-	descripcion.visible = false
-	position += Vector2.UP * -80
-	pass  
+	position = pos_ini
+
+func ocultar_carta(no_jugadas : bool):
+	# false para ocultar todas las cartas
+	# true para ocultar solo las no jugadas
+	if no_jugadas and carta_jugada:
+		pass
+	else:
+		position = pos_ini + Vector2.UP * -500
+		descripcion.visible = false
+
+func subir_carta_mas ():
+	position = pos_ini + Vector2.UP * 200
 
 func _on_carta_mano_pressed():
+	carta_jugada = true
+	subir_carta_mas()
 	self.carta.jugar()
 
 func actualizar_descripcion():
 	descripcion.find_child("titulo").text = self.carta.titulo
 	descripcion.find_child("descripcion").text = self.carta.descripcion
-	pass
 
 func set_carta(_carta):
+	carta_jugada = false
 	self.carta = _carta
 	actualizar_descripcion()
